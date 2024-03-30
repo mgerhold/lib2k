1d4393265ac65a4065585c5da4553f8373ae3897
Michael Gerhold <michael.gerhold@web.de>
2024-03-30 10:05:59 +0100

    Add MemberAccessOperator test and implement `operator->` in `UniqueValue`

--- test/unique_value_tests.cpp
+++ test/unique_value_tests.cpp
@@ -36,7 +36,9 @@ using c2k::UniqueValue;
 
 TEST_F(UniqueValueFixture, CreateUniqueValue) {
     {
-        auto const handle = UniqueValue{ create(), [this](auto const n) { destroy(n); } };
+        auto const handle = UniqueValue{ create(), [this](auto const n) {
+            destroy(n);
+        } };
         EXPECT_GT(*handle, 0);
     }
     EXPECT_TRUE(all_destroyed());
@@ -46,7 +48,9 @@ static_assert(not std::copy_constructible<UniqueValue<int>>);
 
 TEST_F(UniqueValueFixture, MoveConstructible) {
     {
-        auto original_handle = UniqueValue{ create(), [this](auto const handle) { destroy(handle); } };
+        auto original_handle = UniqueValue{ create(), [this](auto const handle) {
+            destroy(handle);
+        } };
         EXPECT_GT(*original_handle, 0);
         auto const underlying_handle = *original_handle;
         auto new_handle = std::move(original_handle);
@@ -57,7 +61,9 @@ TEST_F(UniqueValueFixture, MoveConstructible) {
 }
 
 TEST_F(UniqueValueFixture, VectorOfUniqueValues) {
-    auto const deleter = [this](auto const handle) { destroy(handle); };
+    auto const deleter = [this](auto const handle) {
+        destroy(handle);
+    };
     using Deleter = decltype(deleter);
     {
         auto handles = std::vector<UniqueValue<int, Deleter>>{};
@@ -76,11 +82,11 @@ TEST(UniqueValueTests, UniqueValueWithNonCopyableInside) {
 TEST_F(UniqueValueFixture, DestroyIsCalledOnInvalidHandle) {
 #ifdef _WIN32
     EXPECT_DEATH(
-            {
-                std::ignore = (UniqueValue{ 42, [this](auto const handle) { destroy(handle); } });
-            },
-            // MSVC doesn't show the exception message
-            ""
+        {
+        std::ignore = (UniqueValue{ 42, [this](auto const handle) { destroy(handle); } });
+        },
+        // MSVC doesn't show the exception message
+        ""
     );
 #else
     EXPECT_DEATH(
@@ -90,4 +96,12 @@ TEST_F(UniqueValueFixture, DestroyIsCalledOnInvalidHandle) {
             "trying to destroy non-existing handle"
     );
 #endif
+}
+
+TEST(UniqueValueTests, MemberAccessOperator) {
+    auto value = UniqueValue{ std::string{ "test" } };
+    EXPECT_EQ(value->length(), 4);
+    auto const value2 = std::move(value);
+    EXPECT_THROW(std::ignore = value->length(), std::bad_optional_access);
+    EXPECT_EQ(value2->length(), 4);
 }
